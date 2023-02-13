1fc627208b8eaf7d94000f152fb0112000660a1f
Ilya Sereda <ilyasereda121@gmail.com>
2023-02-13 17:14:12 +0200

    Added transfer status display on UI

--- second_task/src/components/TransferSection/TransferSection.jsx
+++ second_task/src/components/TransferSection/TransferSection.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { ethers } from 'ethers'
 
 
@@ -6,14 +6,24 @@ import './TransferSection.css'
 
 const TransferSection = () => {
 
+    const [error, setError] = useState(false);
+    const [transfered, setTransfered] = useState(false);
 
     const contractAddress = "0xF989661f74eE1541D98277B9a1315e4ea5EE09f0";
     const contractABI = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }], "name": "decreaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" }], "name": "increaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address[]", "name": "accounts", "type": "address[]" }, { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }], "name": "transferTask", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }]
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
     const coins = useRef();
     const address = useRef();
+
+    const onInputChangeHandler = () => {
+        if (error === true)
+            setError(false);
+        if (transfered === true)
+            setTransfered(false);
+    }
 
     const onTransferHandler = (walletAddress, coinsAmount) => {
         const addressArray = walletAddress.split('\n');
@@ -42,7 +52,7 @@ const TransferSection = () => {
                 return;
             }
             if (contract)
-                contract.transferTask(addressArray, coinsAmountArray);
+                contract.transferTask(addressArray, coinsAmountArray).then((result) => setTransfered(true)).catch((error) => setError(true));
         }
 
 
@@ -54,12 +64,14 @@ const TransferSection = () => {
             <div className="form">
                 <div className="inputSection">
                     <label htmlFor='walletAddress' className='label'>Set wallet numbers(each from new string): </label>
-                    <textarea id="walletAddress" className='input' ref={address} ></textarea>
+                    <textarea id="walletAddress" onChange={onInputChangeHandler} className='input' ref={address} ></textarea>
                 </div>
                 <div className="inputSection">
                     <label htmlFor='coinsAmount' className='label'>Set transfer amounts(each from new string): </label>
-                    <textarea id="coinsAmount" type="text" className='input' ref={coins}></textarea>
+                    <textarea id="coinsAmount" onChange={onInputChangeHandler} type="text" className='input' ref={coins}></textarea>
                 </div>
+                {error ? <span className='error'>Transfer amount exceeds balance of transfer was canceled</span> : null}
+                {transfered ? <span className='success'>Transfered successfully. Update page to see current balance</span> : null}
                 <button className='btn' onClick={() => { onTransferHandler(address.current.value, coins.current.value) }}>Send money</button>
             </div>
         </div>
