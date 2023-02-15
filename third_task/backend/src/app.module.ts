1b9e97f88c343dac8fd138f65eb3f0f5d71218c4
Ilya Sereda <ilyasereda121@gmail.com>
2023-02-15 20:18:43 +0200

    done connecting to DB

--- third_task/backend/src/app.module.ts
+++ third_task/backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { TransferModule } from './transfers/transfer.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: 'schema.gpl',
       sortSchema: true,
@@ -15,22 +15,22 @@ import { TransferModule } from './transfers/transfer.module';
       driver: ApolloDriver,
     }),
     TransferModule,
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (config: ConfigService) => ({
-    //     type: 'postgres',
-    //     host: config.get<string>('TYPEORM_HOST'),
-    //     username: config.get<string>('TYPEORM_USERNAME'),
-    //     password: config.get<string>('TYPEORM_PASSWORD'),
-    //     database: config.get<string>('TYPEORM_DATABASE'),
-    //     port: config.get<number>('TYPEORM_PORT'),
-    //     entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
-    //     synchronize: true,
-    //     autoLoadEntities: true,
-    //     logging: true,
-    //   }),
-    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
   ],
 })
 export class AppModule {}
